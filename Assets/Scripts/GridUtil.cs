3f62178e51cd3b1301a6a477a20197e9d91c60cb
Muthu <muthu.rk0914@gmail.com>
2020-10-23 03:02:31 +0530

    Grid Movement Initial Commit -Player can move by swipe in touch input -Player will Loop back and forth from the screens -Dynamic Grid is implemented, Width and Height can be adjusted.

---
+++ Assets/Scripts/GridUtil.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GridUtil : MonoBehaviour {
+
+    [SerializeField]
+    private int rows;
+    [SerializeField]
+    private int cols;
+    [SerializeField]
+    private Vector2 gridSize;
+    [SerializeField]
+    private Vector2 gridOffset;
+
+    [SerializeField]
+    private Sprite cellSprite;
+    private Vector2 cellSize;
+    private Vector2 cellScale;
+
+    void Awake() {
+        GenerateGrid(); //Initialize Grid
+	}
+
+    void GenerateGrid() {
+        GameObject cellObject = new GameObject();
+        cellObject.AddComponent<SpriteRenderer>().sprite = cellSprite;
+        
+        //set color using Hex color codes
+        Color color;
+        if(ColorUtility.TryParseHtmlString("#0D4CD4",out color)){
+            cellObject.GetComponent<SpriteRenderer>().color = color;
+        }
+
+        //scaling to fit the grid
+        Vector2 newCellSize = new Vector2(gridSize.x / (float)cols, gridSize.y / (float)rows);
+        cellSize = cellSprite.bounds.size;
+        cellScale.x = newCellSize.x / cellSize.x;
+        cellScale.y = newCellSize.y / cellSize.y;
+        cellSize = newCellSize; 
+        cellObject.transform.localScale = new Vector2(cellScale.x, cellScale.y);
+
+        //offset for grid cells
+        gridOffset.x = -(gridSize.x / 2) + cellSize.x / 2;
+        gridOffset.y = -(gridSize.y / 2) + cellSize.y / 2;
+
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+                Vector2 pos = new Vector2(col * cellSize.x + gridOffset.x, row * cellSize.y + gridOffset.y);
+                
+                GameObject cO = Instantiate(cellObject, pos, Quaternion.identity) as GameObject;
+
+                cO.transform.parent = transform;
+            }
+        }
+
+        Destroy(cellObject);
+    }
+
+    public Vector2 getCellSize(){
+        return cellSize;
+    }
+    
+    //Grid Width,Height in Editor View
+    void OnDrawGizmos() {
+        Gizmos.DrawWireCube(transform.position, gridSize);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,153 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private float MAX_SWIPE_TIME = 0.5f;
+    float MIN_SWIPE_DISTANCE = 0.17f;
+    float startTime;
+    Vector2 startPos;
+    Vector2 currentDirection = Vector2.zero;
+    Vector2 newDirection = Vector2.zero;
+    Quaternion newRotation;
+
+    float speed = 4.5f;
+
+    bool isMoving;
+    bool playerInsideScreen = true;
+    Vector2 originPos;
+    Vector2 targetPos;
+    float timeToMove = 0.2f;
+    float leftConstraint = Screen.width;
+    float rightConstraint = Screen.width;
+    float bottomConstraint = Screen.height;
+    float topConstraint = Screen.height;
+    Vector2 cellSize;
+
+    void Start(){
+        //Screen constraints
+        Camera cam = Camera.main;
+        float distanceZ = Mathf.Abs(cam.transform.position.z + transform.position.z);
+
+        leftConstraint = cam.ScreenToWorldPoint(new Vector3(0.0f, 0.0f, distanceZ)).x;
+        rightConstraint = cam.ScreenToWorldPoint(new Vector3(Screen.width, 0.0f, distanceZ)).x;
+        bottomConstraint = cam.ScreenToWorldPoint(new Vector3(0.0f, 0.0f, distanceZ)).y;
+        topConstraint = cam.ScreenToWorldPoint(new Vector3(0.0f, Screen.height, distanceZ)).y;
+
+        //cell size of the grid
+        cellSize = GameObject.Find("GridManager").GetComponent<GridUtil>().getCellSize();
+    }
+
+    void Update()
+    {   
+        //Screen transition Check and Swap
+        if (transform.position.x < leftConstraint) {
+            playerInsideScreen = false;
+            if(currentDirection==Vector2.left)
+            transform.position = new Vector3(Mathf.Abs(transform.position.x), transform.position.y, transform.position.z);
+        }
+        else if (transform.position.x > rightConstraint) {
+            playerInsideScreen = false;
+            if(currentDirection==Vector2.right)
+            transform.position = new Vector3(-transform.position.x, transform.position.y, transform.position.z);
+        }
+        else if (transform.position.y < bottomConstraint) {
+            playerInsideScreen = false;
+            if(currentDirection==Vector2.down)
+            transform.position = new Vector3(transform.position.x, Mathf.Abs(transform.position.y), transform.position.z);
+        }
+        else if (transform.position.y > topConstraint) {
+            playerInsideScreen = false;
+            if(currentDirection==Vector2.up)
+            transform.position = new Vector3(transform.position.x, -transform.position.y, transform.position.z);
+        }else{
+            playerInsideScreen = true;
+        }
+
+
+        //Swipe Input
+        if(Input.touches.Length > 0 && playerInsideScreen){
+            Touch t = Input.GetTouch(0);
+            if(t.phase == TouchPhase.Began){
+                startPos = t.position;
+                startTime = Time.time;
+            }
+            if(t.phase == TouchPhase.Ended){
+                if(Time.time - startTime > MAX_SWIPE_TIME)
+                return;
+
+                Vector2 endPos = t.position;
+                Vector2 swipe = new Vector2(endPos.x-startPos.x,endPos.y-startPos.y);
+
+                if(swipe.magnitude < MIN_SWIPE_DISTANCE)
+                return;
+
+                if(Mathf.Abs(swipe.x) > Mathf.Abs(swipe.y)){
+                    if(swipe.x > 0){
+                        newRotation = Quaternion.Euler(0,0,-90);
+                        newDirection = Vector2.right;
+                    }else{
+                        newRotation = Quaternion.Euler(0,0,90);
+                        newDirection = Vector2.left;
+                    }
+                }else{
+                    if(swipe.y > 0){
+                        newRotation = Quaternion.Euler(0,0,0);
+                        newDirection = Vector2.up;
+                    }else{
+                        newRotation = Quaternion.Euler(0,0,180);
+                        newDirection = Vector2.down;
+                    }
+                }
+            }
+        }
+
+        //WASD Input
+        if(!isMoving && playerInsideScreen){
+            if(Input.GetKey(KeyCode.W)){
+                newRotation = Quaternion.Euler(0,0,0);
+                newDirection = Vector2.up;
+            }
+            if(Input.GetKey(KeyCode.A)){
+                newRotation = Quaternion.Euler(0,0,90);
+                newDirection = Vector2.left;
+            }
+            if(Input.GetKey(KeyCode.S)){
+                newRotation = Quaternion.Euler(0,0,180);
+                newDirection = Vector2.down;
+            }
+            if(Input.GetKey(KeyCode.D)){
+                newRotation = Quaternion.Euler(0,0,-90);
+                newDirection = Vector2.right;
+            }
+        }
+
+        //Movement Loop
+        if(!isMoving){
+            if(!newDirection.Equals(currentDirection)){
+                currentDirection = newDirection;
+                transform.rotation = newRotation;
+            }
+            StartCoroutine(Move());
+        }
+    }
+
+
+    IEnumerator Move(){//Movement Logic
+        isMoving = true;
+        float elaspedTime = 0;
+        originPos = transform.position;
+        targetPos = originPos + currentDirection * cellSize;
+        float distance = Vector2.Distance(originPos,targetPos);
+        timeToMove = distance/speed;//varies on distance
+
+        while(elaspedTime<timeToMove){
+            transform.position = Vector2.Lerp(originPos,targetPos,(elaspedTime/timeToMove));
+            elaspedTime += Time.deltaTime;
+            yield return null;
+        }
+        transform.position = targetPos;
+        isMoving = false;
+    }
+}
